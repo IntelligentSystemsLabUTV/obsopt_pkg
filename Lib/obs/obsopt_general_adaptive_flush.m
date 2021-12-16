82c9472b71a2dbc9bb782011cb486c130ebce7a0
Federico Oliva <lvofrc95@gmail.com>
2021-12-16 09:27:24 +0100

    GNU license

--- Lib/obs/obsopt_general_adaptive_flush.m
+++ Lib/obs/obsopt_general_adaptive_flush.m
@@ -1,3 +1,4 @@
+%%%%%%% LICENSING %%%%%%%
 % Copyright 2020-2021 Federico Oliva
 % This program is free software: you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
@@ -11,7 +12,9 @@
 
 % You should have received a copy of the GNU General Public License
 % along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%%%%%%%% CODE %%%%%%%%%%%%
 %% class definition - obsopt observer
 % This class allows to apply a newton-like observer to a specified model,
 % within a model integration setup in the form:
