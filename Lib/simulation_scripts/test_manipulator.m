6e0e3a87f7aff76228f3b5384109c2e011f52c09
fedeoli <lvofrc95@gmail.com>
2022-03-15 09:05:43 +0100

    update test manipulator with input within model

--- Lib/simulation_scripts/test_manipulator.m
+++ Lib/simulation_scripts/test_manipulator.m
@@ -8,14 +8,14 @@ function test = test_manipulator(obs,Ntraj)
     test.time = test.t0:test.tstep:test.tend;
     test.Niter = length(test.time);
     test.Ntraj = Ntraj;
-    test.bound_delta_x = 5e-2*[-1,1]*1;
+    test.bound_delta_x = 1e-1*[-1,1]*1;
     test.bound_delta_x_dot = 1e-2*[-1,1]*0;
     
     for traj=1:test.Ntraj
         % init 
         test.X(traj).val(:,1) = obs.init.X(1).val(1:obs.init.params.dim_state,1);
-%         test.X_est(traj).val(:,1) = obs.init.X(1).val(1:obs.init.params.dim_state,1); 
-        test.X_est(traj).val(:,1) = obs.init.X_est(1).val(1:obs.init.params.dim_state,1); 
+        test.X_est(traj).val(:,1) = obs.init.X(1).val(1:obs.init.params.dim_state,1); 
+%         test.X_est(traj).val(:,1) = obs.init.X_est(1).val(1:obs.init.params.dim_state,1); 
         
         % rand vals
         rand_vals = 1*[unifrnd(test.bound_delta_x(1),test.bound_delta_x(2),2,1); unifrnd(test.bound_delta_x_dot(1),test.bound_delta_x_dot(2),2,1)];
@@ -26,16 +26,17 @@ function test = test_manipulator(obs,Ntraj)
         for i = 1:test.Niter
 
             if i>1
-%                 drive = obs.drive(test.X_est(traj).val(:,i-1),i-1);
-                drive = obs.drive(test.X(traj).val(:,i-1),test.X_est(traj).val(:,i-1));
-                obs.init.params.u = obs.setup.params.input(test.t0,drive,obs.init.params);
+                
+                startpos = i-1;
+                stoppos = i;
+                tspan = test.time(startpos:stoppos);  
 
                 % true system
-                X = obs.setup.ode(@(t,x)obs.setup.model_reference(t, x, obs.init.params), [test.t0 test.tstep], test.X(traj).val(:,i-1),obs.setup.ode);   
+                X = obs.setup.ode(@(t,x)obs.setup.model_reference(t, x, obs.init.params), tspan, test.X(traj).val(:,startpos),obs.setup.ode);   
                 test.X(traj).val(:,i) = X.y(1:obs.init.params.dim_state,end);
 
                 % real system
-                X = obs.setup.ode(@(t,x)obs.setup.model(t, x, obs.init.params), [test.t0 test.tstep], test.X_est(traj).val(:,i-1),obs.setup.ode);
+                X = obs.setup.ode(@(t,x)obs.setup.model(t, x, obs.init.params, obs), tspan, test.X_est(traj).val(:,startpos),obs.setup.ode);
                 test.X_est(traj).val(:,i) = X.y(1:obs.init.params.dim_state,end);
             end
 
