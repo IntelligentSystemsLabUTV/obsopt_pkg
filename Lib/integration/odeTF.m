4a755e10ff2302d1456554c73b0cf20eb2cc73d6
fedeoli <lvofrc95@gmail.com>
2022-06-22 11:36:08 +0200

    feat: differentiate integration with discrete and TF

--- Lib/integration/odeTF.m
+++ Lib/integration/odeTF.m
@@ -8,7 +8,7 @@
 % varargin: general parameters
 % OUTPUT:
 % out: structure with integration results
-function out = odeDD(varargin)
+function out = odeTF(varargin)
 
 % init params
 nargin = length(varargin);
@@ -22,8 +22,11 @@ tspan = varargin{2};
 % y0: initial condition
 x0 = varargin{3};
 
-% integrations options
-opts = varargin{4};
+% U: input
+U = varargin{4};
+
+% TF: transfer function
+TF = varargin{5};
 
 % number of state's components
 N = length(x0);           
@@ -37,15 +40,17 @@ t0 = tspan(1);
 % Matrices allocation
 X = zeros(N,M);
 X(:,1) = x0;
+Y(:,1) = 0*U(:,1);
 
 % integration
 for i = 1:M-1
     
     % DD step
-    X(:,i+1) = feval(ode,tspan(i),X(:,i));   
+    [Y(:,i+1),X(:,i+1)] = feval(ode,tspan(i),X(:,i),U,TF.A, TF.B, TF.C, TF.D);   
     
 end
 
 % store
-out.y = X;
+out.x = X;
+out.y = Y(:,2:end);
 end
