6ba48923028d085bbbd10d0e82f85f6cdcded51d
Federico Oliva <lvofrc95@gmail.com>
2022-07-11 17:46:11 +0200

    chore: plot after test obs

--- Lib/various/plot_general.m
+++ Lib/various/plot_general.m
@@ -2,10 +2,12 @@
 % file: simulation_general_v3.m
 % author: Federico Oliva
 % date: 10/01/2022
-% description: function to setup and use the MHE observer on general model
+% description: function to setup and use the MHE out.observer on general model
 % INPUT: none
-% OUTPUT: params,obs
-function plot_general(out,obs)
+% OUTPUT: params,out.obs
+function plot_general(out)
+
+    
 
     % init section
     i = 0;
@@ -15,36 +17,46 @@ function plot_general(out,obs)
     i = i + 1;            
     figure(i)
     sgtitle('State')    
-    for j=1:length(obs.setup.plot_vars)
-        subplot(length(obs.setup.plot_vars),1,j);
+    for j=1:length(out.obs.setup.plot_vars)
+        subplot(length(out.obs.setup.plot_vars),1,j);
         hold on
         grid on
         box on
-        plot(out.time,out.x_true.val(obs.setup.plot_vars(j),:),'b');
+        plot(out.time,out.x_true.val(out.obs.setup.plot_vars(j),:),'b','LineWidth',1.5);
         for n=1:N
-            plot(out.time,out.x_dist(n).val(obs.setup.plot_vars(j),:),'r--');           
+            plot(out.time,out.x_dist(n).val(out.obs.setup.plot_vars(j),:),'r:','LineWidth',0.8);           
         end
         % labels
         xlabel(['time [s]'])
-        ylabel(['x_',num2str(obs.setup.plot_vars(j))])
+        ylabel(['x_',num2str(out.obs.setup.plot_vars(j))])
         legend('ref','test')
     end
     
     % output plot    
     i = i + 1;            
     figure(i)
-    sgtitle('Output')        
-    hold on
-    grid on
-    box on
-    plot(out.time,reshape(out.y_true.val(1,1,:),1,size(out.y_true.val,3)),'b');
-    for n=1:N
-        plot(out.time,reshape(out.y_dist(n).val(1,:,:),1,size(out.y_dist(n).val,3)),'r--');           
+    sgtitle('Measurements')
+    % number fo subplots depending on the Nterm
+    n_subplot = out.obs.setup.Nfilt+1;
+    for k=1:n_subplot
+        % indicize axes
+        ax_index = k;
+        ax(ax_index)=subplot(n_subplot,1,ax_index);
+                
+        hold on
+        grid on
+        box on
+        
+        plot(out.time,reshape(out.y_true.val(k,1,:),1,size(out.y_true.val,3)),'b','LineWidth',1.5);
+        for n=1:N
+            plot(out.time,reshape(out.y_dist(n).val(k,:,:),1,size(out.y_dist(n).val,3)),'r:','LineWidth',0.8);           
+        end        
+%         set(gca, 'YScale', 'log')
+        % labels
+        xlabel(['time [s]'])
+        ylabel('y')
+        legend('ref','test')
     end
-    % labels
-    xlabel(['time [s]'])
-    ylabel('y')
-    legend('ref','test')
     
     % error plot    
     i = i + 1;            
