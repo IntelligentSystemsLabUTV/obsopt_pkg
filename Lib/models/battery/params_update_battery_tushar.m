0ef35dcf694c7bc6cd5b2a66c02832e239b70681
fedeoli <lvofrc95@gmail.com>
2022-08-30 18:11:43 +0200

    feat: add update for all the params

--- Lib/models/battery/params_update_battery_tushar.m
+++ Lib/models/battery/params_update_battery_tushar.m
@@ -13,8 +13,25 @@ function params_out = params_update_battery_tushar(params,x,t)
     % assign params
     params_out = params;          
 
-%     params_out.Voc = x(3,:);
-%     params_out.R0 = x(4,:);
-%     params_out.R1 = x(5,:);
-%     params_out.C1 = x(6,:);
+    params_out.Voc = x(3,:);
+    params_out.R0 = x(4,:);
+    params_out.R1 = x(5,:);
+    params_out.C1 = x(6,:);
+
+    params_out.alpha_Voc = x(7,:);
+    params_out.alpha_R0 = x(8,:);
+    params_out.alpha_R1 = x(9,:);
+    params_out.alpha_C1 = x(10,:);
+    
+    params_out.beta_Voc = x(11,:);
+    params_out.beta_R0 = x(12,:);
+    params_out.beta_R1 = x(13,:);
+    params_out.beta_C1 = x(14,:);
+    
+    params_out.gamma_Voc = x(15,:);
+    params_out.gamma_R0 = x(16,:);
+    params_out.gamma_R1 = x(17,:);
+    params_out.gamma_C1 = x(18,:);
+    
+    
 end
