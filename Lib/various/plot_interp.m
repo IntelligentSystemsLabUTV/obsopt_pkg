d2970c373813edd0edfd6c2afd6e76e4f1c11f47
Federico Oliva <lvofrc95@gmail.com>
2022-09-06 10:47:14 +0200

    chore: improve plot interp (first guess)

--- Lib/various/plot_interp.m
+++ Lib/various/plot_interp.m
@@ -1,33 +1,47 @@
 %% function 
-function plot_interp(obs,str,pos,params,wholeSim)
+function plot_interp(obs,str,pos,params,wholeSim,fig)
+
+    
 
     % create data
-    if ~wholeSim
-        data = obs.init.cloud(1,:);
-    else
-        data = obs.init.X_est(1).val(1,:);
-    end
+    data = obs.init.cloud(1,:);
+%     data = obs.init.X_est(1).val(1,:);
+    
+    % sort in ascending order
+    [data_s, idx_s] = sort(data);
     
     % compute poly
     for p=1:length(params)
-       data_p(p,:) = data.^(p-1); 
+       data_p(p,:) = data_s.^(p-1); 
     end
 
     % plot
-    figure()
-    plot(obs.init.params.input_soc,str)
+    figure(fig)
+%     plot(obs.init.params.input_soc,str,'LineWidth',2)
     hold on
     
     if ~wholeSim
-        i=1;
-        plot(obs.init.cloud(1,:),spline(obs.init.params.input_soc, str, obs.init.cloud(1,:)),'ro')
-        plot(obs.init.cloud(1,:),obs.init.cloud(pos,:),'r+')
-        plot(obs.init.X(1).val(1,i),spline(obs.init.params.input_soc, str, obs.init.X(1).val(1,i)),'ko')                        
+        i=1;        
+        plot(data_s,obs.init.cloud(pos,idx_s),'r+','LineWidth',2)
+        plot(data_s,spline(obs.init.params.input_soc, str, obs.init.cloud(1,idx_s)),'b:o','LineWidth',2)
+%         plot(obs.init.X(1).val(1,i),spline(obs.init.params.input_soc, str, obs.init.X(1).val(1,i)),'k+')   
+
+        % plot estimation
+        plot(data_s,params*data_p,'k--o','LineWidth',2)
     else
-        plot(obs.init.X(1).val(1,:),spline(obs.init.params.input_soc, str, obs.init.X(1).val(1,:)),'ro')        
+%         plot(obs.init.X(1).val(1,:),spline(obs.init.params.input_soc, str, obs.init.X(1).val(1,:)),'m--o','LineWidth',2)    
+        
+        % plot estimation
+        plot(data_s,params*data_p,'m--o','LineWidth',2)
     end
     
-    % plot estimation
-    plot(data,params*data_p,'k+')
+    
+    
+    % manipulate
+    grid on
+    xlabel('Z')
+    ylabel('param')
+%     legend('experimental data')
+    legend('measured','spline interp','linear interp')
 
 end
